37e7b4e94d123858fc2fd483d6453d6e1e7daeeb
Lee Joon Ho <jh.lee@spira-t.com>
2021-03-10 14:45:54 +0900

    nobot1, 2

--- examples/001/hello.js
+++ examples/001/hello.js
@@ -1,9 +1,9 @@
 const args = process.argv.slice(2);
-const [name] = args;
+const [name1] = args;
 
-if (name === undefined) {
+if (name1 === undefined) {
   console.error('Please pass a name, e.g. node hello.js Shaun');
   process.exit(0);
 }
 
-console.log(`Good day to you, ${name}`);
+console.log(`Good day to you, ${name1}`);
